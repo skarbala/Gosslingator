52816e8336e65eb824140b694b80e5aab9055c63
martin.skarbala <skarbala.martin@gmail.com>
2018-11-28 12:46:29 +0100

    replaced css with sass added functionality for removing specific ryan extracted css and js from index.html

---
+++ cypress/integration/main.js
@@ -0,0 +1,17 @@
+before('open main page', () => cy.visit('http://localhost:8888'));
+describe('Adding ryans', () => {
+    it('should add one ryan ', () => {
+        cy.get('#addRyan').click();
+        cy.get('img').should('be.visible');
+    });
+    it('should enable remove button', () => {
+        cy.get('#removeRyan')
+            .should('be.enabled');
+    });
+    it('should remove ryan', () => {
+        cy.get('#removeRyan')
+            .click()
+            .should('be.disabled');
+        cy.get('img').should('not.exist');
+    });
+});
